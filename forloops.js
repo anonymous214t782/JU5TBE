0e0f74180a60454d1c4c47374495995cec6e7fdc
JU5TBE <uwdursstw32w@gmail.com>
2025-04-29 15:48:27 +0200

    Create forloops.js

---
+++ forloops.js
@@ -0,0 +1,28 @@
+for (let count = 1; count < 11; count += 1) {
+    console.log(count)
+}
+// for -- for loop
+//count = 1 -- start count
+//count < 11 -- limit/endcount
+//count += 1 -- count in 1s
+
+//for loop that counts from 10 to 100 in steps of ten
+for (let count = 10; count < 101; count += 10){
+    console.log(count)
+}
+
+let messages = [
+    "hey hows it going",
+    "recoding this is really annoying",
+    "cooooode",
+    "last text"
+]
+
+for (let i = 0; i < length; i += 1){
+    console.log(messages[i])
+}
+
+let cards = [7,8,9]
+for (i = 0; i < length; i += 1 ){
+    console.log(cards[i])
+}
