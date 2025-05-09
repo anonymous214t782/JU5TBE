4a56e1b1f1f9eb4ce78f18ee5b11a2a211c16283
JU5TBE <uwdursstw32w@gmail.com>
2025-05-09 15:14:17 +0200

    Create lessons .js

---
+++ lessons .js
@@ -0,0 +1,24 @@
+//blackjack game lessons
+//1.ARRAYS 
+//2.objects
+//3.booleans
+//4.if, else if, and else statements....fucken important
+//comparison operators:>,<,===, operators
+//logical operators: checks if conditions are true and runs codes
+//For loops 
+//math.... for fucks sake
+//Math.floor(Math.random()*13)+1
+//return stuff
+
+let person = {
+
+    name: 'Nobody',
+    age: 1,
+    country: "Afganistan" 
+    
+}
+
+function logData(){
+    console.log(person.name + " is " + person.age + " years old and lives in " + person.country)
+    
+}
