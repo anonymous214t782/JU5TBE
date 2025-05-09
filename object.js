9b7bbdc8eeb7cb02f7431bb1aa121bc3ee0703c3
JU5TBE <uwdursstw32w@gmail.com>
2025-05-09 15:12:30 +0200

    Create object.js

---
+++ object.js
@@ -0,0 +1,37 @@
+//git upload for objecct . js typescript
+// for  the object concept in coding 
+//objects store data in depth and are called complex/composite data types
+//CAN EVEN STORE ARRAYS (SO YAH COMPLEX STRUCTURES FOR ONE TYPE OF STUFF 
+//CAN BE STORED IN AN OBJECT)
+
+let course = {
+    title: "Use of Objects",
+    length: 10000,
+    level: 1,//per has to justify why begginer in html has to have a number here
+    //Justification:normal in real world cases
+    creator: "atoms",
+    lessons: "none",
+    isFree: true,
+    tags: ["javascript", "free"],
+    startgame: function() {
+        console.log("functions can be stored here too")
+    }
+}
+
+console.log(course.creator)
+
+
+//airbnb challenge
+//object, airbnb listing, contain one boolean one string one nuber and one variable 
+//log out two keys minimal
+
+castleOne = {
+    name: "olympus",
+    price: 59999999,
+    available: true,
+    images: [img/castl1.jpg,img/catle2.png]
+}
+
+console.log(catleOne.price)
+console.log(catleOne.available)
+
