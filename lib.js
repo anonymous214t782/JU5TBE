252a2c553ef817a6f7d877d5b41b830685c5428b
anonymous214t782 <uwdursstw32w@gmail.com>
2025-06-03 16:00:24 +0200

    pls upload

--- lib.js
+++ lib.js
@@ -1,3 +1,6 @@
+
+
+
 // contact me info
         const contact_me = document.getElementById("contact_me");
         const contact_info = document.getElementById("contact_info");
@@ -19,3 +22,60 @@ contact_me.addEventListener("click", function(){
 
 
 //testing
+
+ const contact_me1 = document.getElementById("contact_me1");
+        const contact_info1 = document.getElementById("contact_info1");
+        
+        
+        // Ensure the elements exist before accessing them
+         // Skip iteration if any element is missing
+        
+        // Toggle visibility based on the current state of the dots
+contact_me1.addEventListener("click", function(){
+    if (contact_info1.style.display === "none"){
+            contact_info1.style.display = "inline";                   
+    }else{
+            contact_info1.style.display = "none";
+    }
+    })
+
+// contact me info
+
+
+//testing
+const contact_me2 = document.getElementById("contact_me2");
+        const contact_info2 = document.getElementById("contact_info2");
+        
+        
+        // Ensure the elements exist before accessing them
+         // Skip iteration if any element is missing
+        
+        // Toggle visibility based on the current state of the dots
+contact_me2.addEventListener("click", function(){
+    if (contact_info2.style.display === "none"){
+            contact_info2.style.display = "inline";                   
+    }else{
+            contact_info2.style.display = "none";
+    }
+    })
+
+// contact me info
+
+const contact_me3 = document.getElementById("contact_me3");
+        const contact_info3 = document.getElementById("contact_info3");
+        
+        
+        // Ensure the elements exist before accessing them
+         // Skip iteration if any element is missing
+        
+        // Toggle visibility based on the current state of the dots
+contact_me3.addEventListener("click", function(){
+    if (contact_info3.style.display === "none"){
+            contact_info3.style.display = "inline";                   
+    }else{
+            contact_info3.style.display = "none";
+    }
+    })
+
+// contact me info
+
