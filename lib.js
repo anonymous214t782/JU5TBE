29c9ccb86bcb34314bcfd8eba77aff06127eee5c
Nkosiyapha Zulu <uwdursstw32w@gmail.com>
2025-06-04 13:08:31 +0200

    added information for about us and profile,still missing corporate governance and some pictures

--- lib.js
+++ lib.js
@@ -1,14 +1,14 @@
 
-
-
-// contact me info
-        const contact_me = document.getElementById("contact_me");
-        const contact_info = document.getElementById("contact_info");
-        
+const contact_me = document.querySelector(".contact_me");
+const contact_info = document.querySelector(".contact_info");
+const close_contact = document.querySelector(".close_contact"); 
+const contact_info_profile = document.querySelector(".contact_info_profile");
         
         // Ensure the elements exist before accessing them
          // Skip iteration if any element is missing
-        
+close_contact.addEventListener("click", function(){
+      contact_info.style.display = "none";
+})       
         // Toggle visibility based on the current state of the dots
 contact_me.addEventListener("click", function(){
     if (contact_info.style.display === "none"){
@@ -16,66 +16,13 @@ contact_me.addEventListener("click", function(){
     }else{
             contact_info.style.display = "none";
     }
+
+    if (contact_info_profile.style.display === "none"){
+            contact_info_profile.style.display = "inline";                   
+    }else{
+            contact_info_profile.style.display = "none";
+    }
     })
 
 // contact me info
-
-
 //testing
-
- const contact_me1 = document.getElementById("contact_me1");
-        const contact_info1 = document.getElementById("contact_info1");
-        
-        
-        // Ensure the elements exist before accessing them
-         // Skip iteration if any element is missing
-        
-        // Toggle visibility based on the current state of the dots
-contact_me1.addEventListener("click", function(){
-    if (contact_info1.style.display === "none"){
-            contact_info1.style.display = "inline";                   
-    }else{
-            contact_info1.style.display = "none";
-    }
-    })
-
-// contact me info
-
-
-//testing
-const contact_me2 = document.getElementById("contact_me2");
-        const contact_info2 = document.getElementById("contact_info2");
-        
-        
-        // Ensure the elements exist before accessing them
-         // Skip iteration if any element is missing
-        
-        // Toggle visibility based on the current state of the dots
-contact_me2.addEventListener("click", function(){
-    if (contact_info2.style.display === "none"){
-            contact_info2.style.display = "inline";                   
-    }else{
-            contact_info2.style.display = "none";
-    }
-    })
-
-// contact me info
-
-const contact_me3 = document.getElementById("contact_me3");
-        const contact_info3 = document.getElementById("contact_info3");
-        
-        
-        // Ensure the elements exist before accessing them
-         // Skip iteration if any element is missing
-        
-        // Toggle visibility based on the current state of the dots
-contact_me3.addEventListener("click", function(){
-    if (contact_info3.style.display === "none"){
-            contact_info3.style.display = "inline";                   
-    }else{
-            contact_info3.style.display = "none";
-    }
-    })
-
-// contact me info
-
