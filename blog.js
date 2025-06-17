d90a4b8d314a4d38c2e4c044aa64554a8c7323f4
Nkosiyapha Zulu <uwdursstw32w@gmail.com>
2025-06-17 11:27:50 +0200

    Create blog.js

---
+++ blog.js
@@ -0,0 +1,5 @@
+const learn_code_genre = document.querySelector(".learn_code")
+const hot_takes = document.querySelector(".hot_takes")
+const tips_and_tricks = document.querySelector(".tnt")
+const rants = document.querySelector(".fucken_rants")
+
